a6a7909f0e283da6dfcc31ac0f1f29874a3cee58
rlawogns1121 <rlawogns1121@gmail.com>
2025-09-24 16:59:27 +0900

    Update mainpage

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/dol.png"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -14,14 +14,13 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            안녕하세요, 저희 고양이입니다. 
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
             </code>
-            .
+            
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            저희 고양이 이름은 흰돌입니다.
           </li>
         </ol>
 
